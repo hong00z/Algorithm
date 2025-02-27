0d736e456ab3eaa526817bcea34aef8abb918efb
mjoo <150416207+hong00z@users.noreply.github.com>
2025-02-27 16:16:05 +0900

    [Silver III] Title: 등수 매기기, Time: 676 ms, Memory: 52608 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] ranks = new int[n];
+        for (int i = 0; i < n; i++) {
+            ranks[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(ranks);  // 예상 등수 정렬
+
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += Math.abs(ranks[i] - (i + 1));
+        }
+
+        System.out.println(ans);
+    }
+}
